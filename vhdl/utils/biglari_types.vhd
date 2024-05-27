a53f7798dddc011e52eb899b9e5bc539cd3343ca
bcho892 <AKLbc70@gmail.com>
2024-05-27 13:47:12 +1200

    wolf compatiability
    Co-authored-by: Nicholas Wolf <wolfuoa@users.noreply.github.com>

--- vhdl/utils/biglari_types.vhd
+++ vhdl/utils/biglari_types.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 
 package BiglariTypes is
     type packet is (config, average_data, correlation_data, invalid);
-    constant data_max_width : integer := 16;
+    constant data_max_width : integer := 20;
     subtype data_width is std_logic_vector(data_max_width - 1 downto 0);
     constant counter_max_width : integer := 28;
     subtype counter_width is unsigned(counter_max_width - 1 downto 0);
