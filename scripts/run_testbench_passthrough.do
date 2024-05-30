2a31912a6cf17eb8af48ceb11778ff7b12d9cbca
bcho892 <AKLbc70@gmail.com>
2024-05-30 16:00:34 +1200

    add testbench for passthrough

---
+++ scripts/run_testbench_passthrough.do
@@ -0,0 +1,57 @@
+
+puts {
+    biglari peak detector TOP LEVEL integration test
+}
+set top_level              test_library.testbench_passthrough
+set wave_patterns {
+        top_level_inst/control_unit/current_state
+        t_clock
+        t_data_in
+        t_data_out
+        t_reset
+        top_level_inst/min_value_storage/registered_min_value
+        top_level_inst/max_value_storage/registered_max_value
+        top_level_inst/packet_decode/packet_type
+        top_level_inst/peak_detection/peak_detected
+}
+
+set wave_radices {
+  hexadecimal {
+        t_data_in
+        t_data_out
+        t_reset
+        registered_min_value
+        registered_max_value
+  }
+}
+
+# load the simulation
+eval vsim $top_level
+
+# if waves are required
+set a [if [llength $wave_patterns] {
+  noview wave
+  foreach pattern $wave_patterns {
+    add wave $pattern
+  }
+  configure wave -signalnamewidth 1
+  foreach {radix signals} $wave_radices {
+    foreach signal $signals {
+      catch {property wave -radix $radix $signal}
+    }
+  }
+}]; list
+
+# run the simulation
+run 1000ns
+
+# if waves are required
+if [llength $wave_patterns] {
+  if $tk_ok {wave zoomfull}
+}
+
+puts {
+  *************************
+  CLLOWNDS SMALLARI
+  *************************
+}
---
+++ test/testbench_passthrough.vhd
@@ -0,0 +1,51 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.ZoranTypes;
+use work.NocConstants;
+
+entity testbench_passthrough is
+end entity;
+
+architecture rtl of testbench_passthrough is
+    signal t_clock    : std_logic;
+    signal t_reset    : std_logic;
+    signal t_data_in  : ZoranTypes.mips_tdma_min_port;
+    signal t_data_out : ZoranTypes.mips_tdma_min_port;
+begin
+    top_level_inst : entity work.top_level_pd_asp
+        port map(
+            clock    => t_clock,
+            reset    => t_reset,
+            data_in  => t_data_in,
+            data_out => t_data_out
+        );
+
+    process
+    begin
+        t_clock <= '1';
+        wait for 10 ns;
+        t_clock <= '0';
+        wait for 10 ns;
+    end process;
+
+    process
+    begin
+        -- enable the PD
+        wait until rising_edge(t_clock);
+        t_data_in.addr <= x"FF";
+        t_data_in.data <= NocConstants.pd_config_code & x"F537FFF";
+        wait until rising_edge(t_clock);
+        t_data_in.data <= NocConstants.average_data_code & x"F537FFF";
+        wait until rising_edge(t_clock);
+        t_data_in.data <= NocConstants.average_data_code & x"EEFF00D";
+        wait until rising_edge(t_clock);
+        t_data_in.data <= NocConstants.average_data_code & x"F999999";
+        wait until rising_edge(t_clock);
+        t_data_in.data <= NocConstants.average_data_code & x"EEEEEEE";
+        wait until rising_edge(t_clock);
+        t_data_in.data <= NocConstants.average_data_code & x"6969696";
+        wait until rising_edge(t_clock);
+        t_data_in.data <= NocConstants.average_data_code & x"9A9A9A9";
+    end process;
+end architecture;
